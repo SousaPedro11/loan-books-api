dc8c6a3461b45dcb582c6e425949b33ad8fb096e
Pedro Sousa <ppls2106@gmail.com>
2021-03-12 13:08:29 -0300

    desativando browsable

--- loan_books/settings.py
+++ loan_books/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
+        # 'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
