e75f5da82f5e5b25dd9612ba68d3408ff67a11e2
Pedro Sousa <ppls2106@gmail.com>
2021-03-12 04:51:41 -0300

    feat(core): Adicionando urls da api
    Adicionando as URLs básicas para o funcionamento do projeto
    
    Adicionado por: sousapedro11

---
+++ library_api/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import SimpleRouter
+from library_api import views
+
+
+router = SimpleRouter()
+
+router.register(r'book', views.BookViewSet)
+router.register(r'client', views.ClientViewSet)
+router.register(r'reservation', views.ReservationViewSet)
+
+urlpatterns = router.urls
