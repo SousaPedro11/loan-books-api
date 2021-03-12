43f1bef962ab78be08b457d04b12610d160420e8
Pedro Sousa <ppls2106@gmail.com>
2021-03-12 05:01:06 -0300

    feat(api): Adicionando paginação
    Adicionando paginação customizada e dinâmica
    
    Adicionado por: sousapedro11

--- library_api/paginators.py
+++ library_api/paginators.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 
 
 class LibraryPaginator(LimitOffsetPagination):
@@ -7,4 +8,37 @@ class LibraryPaginator(LimitOffsetPagination):
 
 
 class CustomPaginator(LibraryPaginator):
-    pass
+    def get_next_link(self):
+        if self.offset + self.limit >= self.count:
+            return None
+
+        url = {'size': self.limit}
+
+        offset = self.offset + self.limit
+        url['offset'] = offset
+        return url
+
+    def get_previous_link(self):
+        if self.offset <= 0:
+            return None
+
+        url = {'size': self.limit}
+
+        if self.offset - self.limit <= 0:
+            url['offset'] = 0
+            return url
+
+        offset = self.offset - self.limit
+        url['offset'] = offset
+        return url
+
+    def get_paginated_response(self, data):
+
+        resposta = {
+            "count": self.count,
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+            "results": data
+        }
+
+        return Response(resposta)
